7689405d64f2d0e8ab5ea82074379a93b2d50174
pioter00 <piotr.pietrzak15@gmail.com>
2022-11-10 12:58:46 +0100

    Add proper request handling

--- lab3/source/server.py
+++ lab3/source/server.py
@@ -3,33 +3,16 @@ import http.server
 import socketserver
 import os
 import datetime as dt
-import pytz
 from urllib.parse import urlparse
 from urllib.parse import parse_qs
 
 class web_server(http.server.SimpleHTTPRequestHandler):
     
     def do_GET(self):
-        now = dt.datetime.now(pytz.timezone('Europe/Warsaw'))
-        time = now.strftime("%H:%M:%S") + "\n"
         parsed = urlparse(self.path)
-        qs = parse_qs(parsed.query)
+        qs = parse_qs(parsed.query)         
 
-        if len(qs) == 0:
-            self.protocol_version = 'HTTP/1.1'
-            self.send_response(200)
-            self.send_header("Content-type", "text/html; charset=UTF-8")
-            self.end_headers()            
-            self.wfile.write(b"Hello World!\n")
-            
-        elif len(qs) == 1 and 'cmd' in qs.keys() and qs['cmd'][0] == 'time':
-            self.protocol_version = 'HTTP/1.1'
-            self.send_response(200)
-            self.send_header("Content-type", "text/html; charset=UTF-8")
-            self.end_headers()
-            self.wfile.write(time.encode())            
-
-        elif len(qs) == 2 and 'cmd' in qs.keys() and qs['cmd'][0] == 'rev' and 'str' in qs.keys():
+        if len(qs) == 1 and 'str' in qs.keys():
             self.protocol_version = 'HTTP/1.1'
             self.send_response(200)
             self.send_header("Content-type", "text/html; charset=UTF-8")

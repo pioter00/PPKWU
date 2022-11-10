38ee39cff660205000bc02331bfb3c51893c235f
pioter00 <piotr.pietrzak15@gmail.com>
2022-11-10 13:12:52 +0100

    Add digit counting

--- lab3/source/server.py
+++ lab3/source/server.py
@@ -19,12 +19,10 @@ class web_server(http.server.SimpleHTTPRequestHandler):
             self.send_header("Content-type", "text/html; charset=UTF-8")
             self.end_headers()
             to_count_str = qs['str']
-
-            lowercase_count = 
             count_dict = { 
                 "lowercase" : sum(map(str.islower, to_count_str)), 
                 "uppercase" : sum(map(str.isupper, to_count_str)), 
-                "digits" : 0, 
+                "digits" : sum(map(str.isdigit, to_count_str)), 
                 "special" : 0
             }
             self.wfile.write(json.dumps(count_dict).encode())

f3c13ea4394ec19113f451d797fd5b17e8608125
pioter00 <piotr.pietrzak15@gmail.com>
2022-11-24 12:47:01 +0100

    add calculations

--- lab4/source/server.py
+++ lab4/source/server.py
@@ -13,22 +13,22 @@ class web_server(http.server.SimpleHTTPRequestHandler):
         parsed = urlparse(self.path)
         qs = parse_qs(parsed.query)         
 
-        if len(qs) == 1 and 'num1' in qs.keys() and 'num2' in qs.keys():
+        if len(qs) == 2 and 'num1' in qs.keys() and 'num2' in qs.keys():
             self.protocol_version = 'HTTP/1.1'
             self.send_response(200)
             self.send_header("Content-type", "text/html; charset=UTF-8")
             self.end_headers()
-            num1 = qs['num1'][0]
-            num2 = qs['num2'][0]
-            if not isinstance(num1, int) or not isinstance(num2, int) or num2 != 0:
+            num1 = int(qs['num1'][0])
+            num2 = int(qs['num2'][0])
+            if num2 == 0:
                 self.wfile.write("Not valid numbers".encode())
             else:
                 count_dict = {    
-                    "sum" : 0, 
-                    "sub" : 0, 
-                    "mul" : 0, 
-                    "div" : 0,
-                    "mod" : 0
+                    "sum" : num1 + num2, 
+                    "sub" : num1 - num2, 
+                    "mul" : num1 * num2, 
+                    "div" : int(num1 / num2),
+                    "mod" : num1 % num2
                 }
                 self.wfile.write(json.dumps(count_dict).encode())
         else:
